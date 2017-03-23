2e0ce3c402cfd73e7429b98caf45495e49cd9219
Jim Apple <jbapple-impala@apache.org>
2017-03-23 08:59:39 -0700

    install more clang; find whereis path

--- build/docker/scripts/ubsan.sh
+++ build/docker/scripts/ubsan.sh
@@ -3,7 +3,10 @@
 set -ex
 
 #sudo apt-get update
-sudo apt-get install -y --no-install-recommends clang-3.8
+sudo apt-get install -y --no-install-recommends clang-3.8 llvm-3.8-dev
+
+whereis clang
+clang --version
 
 CC=clang-3.8
 export CC
@@ -15,6 +18,8 @@ UBSAN_OPTIONS=print_stacktrace=1
 export UBSAN_OPTIONS
 
 CLANG_PATH=$(mktemp -d)
+LLVM_SYMBOLIZER_PATH="$(whereis llvm-symbolizer-3.8)"
+echo $LLVM_SYMBOLIZER_PATH
 ln -s "$(whereis llvm-symbolizer-3.8  | rev | cut -d ' ' -f 1 | rev)" \
   "${CLANG_PATH}/llvm-symbolizer"
 ls -l "${CLANG_PATH}"
