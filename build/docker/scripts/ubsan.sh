147d3eced9e5960ffad901ad484a4873d7c09472
Jim Apple <jbapple-impala@apache.org>
2017-03-22 19:01:54 -0700

    pass FLAGS in env

--- build/docker/scripts/ubsan.sh
+++ build/docker/scripts/ubsan.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -x
+set -ex
 
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get install -y clang-3.8
 
 CC=clang-3.8
@@ -15,11 +15,18 @@ UBSAN_OPTIONS=print_stacktrace=1
 export UBSAN_OPTIONS
 
 CLANG_PATH=$(mktemp -d)
-ln -s "$(whereis llvm-symbolizer-3.8)" "${CLANG_PATH}/llvm-symbolizer"
+ln -s "$(whereis llvm-symbolizer-3.8  | rev | cut -d ' ' -f 1 | rev)" \
+  "${CLANG_PATH}/llvm-symbolizer"
+ls -l "${CLANG_PATH}"
 PATH="${CLANG_PATH}:${PATH}"
 export PATH
 
+#  --without-haxe
+
+CFLAGS='-fsanitize=undefined -fno-sanitize-recover=undefined -fno-sanitize=vptr'
+export CFLAGS
+
+CXXFLAGS='-fsanitize=undefined -fno-sanitize-recover=undefined -fno-sanitize=vptr'
+export CXXFLAGS
+
 build/docker/scripts/autotools.sh \
-  'CFLAGS="-fsanitize=undefined -fno-sanitize-recover=undefined -fno-sanitize=vptr"' \
-  'CXXFLAGS="-fsanitize=undefined -fno-sanitize-recover=undefined -fno-sanitize=vptr"' \
-  --without-haxe
