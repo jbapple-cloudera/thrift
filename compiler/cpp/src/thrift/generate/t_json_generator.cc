e453963bd2dfbaafda98091c8b76bd959d5d8828
Jim Apple <jbapple-impala@apache.org>
2017-03-24 22:16:12 -0700

    clean up in prep for PR

--- compiler/cpp/src/thrift/generate/t_json_generator.cc
+++ compiler/cpp/src/thrift/generate/t_json_generator.cc
@@ -273,14 +273,8 @@ void t_json_generator::write_type_spec(t_type* ttype) {
     write_key_and_string("valueTypeId", get_type_name(vtype));
     write_type_spec_object("keyType", ktype);
     write_type_spec_object("valueType", vtype);
-  } else {
-    t_type* etype = 0;
-    if (ttype->is_list()) {
-      etype = ((t_list*)ttype)->get_elem_type();
-    }
-    if (ttype->is_set()) {
-      etype = ((t_set*)ttype)->get_elem_type();
-    }
+  } else if (ttype->is_list() || ttype->is_set()) {
+    t_type* etype = ((t_list*)ttype)->get_elem_type();
     write_key_and_string("elemTypeId", get_type_name(etype));
     write_type_spec_object("elemType", etype);
   }
